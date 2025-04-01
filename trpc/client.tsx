d38b6da37e0dfea423721301c9e4b4cd3c8d7add
Evans Maina <marsappollo3@gmail.com>
2025-04-01 20:49:34 +0300

    Refactor TRPC client URL handling to differentiate between development and production environments. Update production URL to point to the new Vercel deployment.

--- trpc/client.tsx
+++ trpc/client.tsx
@@ -29,10 +29,10 @@ function getQueryClient() {
 function getUrl() {
   const base = (() => {
     if (typeof window !== "undefined") return "";
-    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-    if (process.env.NEXT_PUBLIC_VERCEL_URL)
-      return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
-    return "http://localhost:3000";
+    if (process.env.NODE_ENV === "development") return "http://localhost:3000";
+    if (process.env.NODE_ENV === "production")
+      return `https://learning-trpc.vercel.app`;
+    return "";
   })();
   return `${base}/api/trpc`;
 }
